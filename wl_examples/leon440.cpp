15b69c0d39b1a6a63aaa1456d434371ef34c8694
LeonCSt <leoncstanley@gmail.com>
2025-11-04 15:12:39 +1100

    Redraw on resize. Add keyboard events.

--- wl_examples/leon440.cpp
+++ wl_examples/leon440.cpp
@@ -173,8 +173,8 @@ void draw() {
 
 void xdgsurf_cnfgr(void *data, xdg_surface *xdgsurf, unsigned serial) {
   if (redraw && (surfwidth != bffwdth || surfheight != bffhght)) {
-    thread t1(draw); t1.detach();
     xdg_surface_ack_configure(xdgsurf, serial);
+    thread t1(draw); t1.detach();
   }
 }
 
