0bb73e3ecd95f3ddc00e715b7aa17c985b25be67
LeonCSt <leoncstanley@gmail.com>
2025-11-26 09:52:58 +1100

    Using the X11 clipboard

--- X11_examples/leon070.cpp
+++ X11_examples/leon070.cpp
@@ -2,7 +2,7 @@
     select, cut and paste text - using the clipboard
     [mouse drag] select     [C] cut     [P] paste or [middlemouse btn]
     example  = "Utf8 encoding L♡VE, j☺y, pe☮ce. Regards ←"
-    g++ leon.cpp -o leon -lX11 -lXft -lfontconfig -I/usr/include/freetype2
+    g++ leon070.cpp -o leon -lX11 -lXft -lfontconfig -I/usr/include/freetype2
                                                           [250707]  */
 
 #include <X11/Xlib.h>
