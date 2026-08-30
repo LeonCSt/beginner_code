7326a5d6fe5f99e7216207fdb7fc24964d838d69
LeonCSt <leoncstanley@gmail.com>
2026-08-30 10:04:55 +1000

    Text rendering with FreeType2

--- X11_examples/leon007.cpp
+++ X11_examples/leon007.cpp
@@ -263,20 +263,17 @@ void init() {
           DefaultVisual(dis, 0), attriMask, &winAttr);
   WM_DELETE_WINDOW = XInternAtom(dis, "WM_DELETE_WINDOW", False);
   XSetWMProtocols(dis, win, &WM_DELETE_WINDOW, 1);
-  XSizeHints sh; XWMHints hnts; XTextProperty nm, icnm;
+  XSizeHints sh; XWMHints hnts;
   sh.flags = PPosition | PSize | PMinSize | PMaxSize | PWinGravity;
   sh.x = left; sh.y = top; sh.width = cfgW; sh.height = cfgH;
   sh.max_width = waW; sh.max_height = waH; 
   sh.min_width = 320; sh.min_height = 300;
   sh.win_gravity = CenterGravity;
   hnts.flags = 1; hnts.input = true;
-  char snm[] = "Test GUI(Title Bar Text)";
-  char *psnm = snm;
-  XStringListToTextProperty(&psnm, 1, &nm);
-  char sicnm[] = "Test GUI(Icon Text)";
-  char *psicnm = sicnm;
-  XStringListToTextProperty(&psicnm, 1, &icnm);
-  XSetWMProperties(dis, win, &nm, &icnm, NULL, 0, &sh, &hnts, NULL);
+  XStoreName(dis, win, "Test GUI(Title Bar Text)");
+  XSetIconName(dis, win, "Test GUI(Icon Text)");
+  XSetWMNormalHints(dis, win, &sh);
+  XSetWMHints(dis, win, &hnts);
                   //Section: Set up shared memory
   shminfo.readOnly = False;
   shminfo.shmid = shmget(IPC_PRIVATE, waW * waH * 4, IPC_CREAT | 0777);
@@ -354,3 +351,4 @@ int main() {
   shmctl(shminfo.shmid, IPC_RMID, 0);
   XDestroyWindow(dis, win);
 }
+
